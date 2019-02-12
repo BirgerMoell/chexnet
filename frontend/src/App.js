f38b08e1b7a290f16244e1b2f0c52805de5ee566
Birger Moëll <birger.moell@gmail.com>
2019-02-12 02:04:33 +0100

    Updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,7 +5,7 @@ import prediction from "./prediction.png";
 import "./App.css";
 import { css } from "@emotion/core";
 import { ClipLoader } from "react-spinners";
-import { PDFReader } from 'react-read-pdf';
+import { PDFReader } from "react-read-pdf";
 
 const override = css`
   display: block;
@@ -36,24 +36,24 @@ class App extends Component {
     });
 
     let file = this.state.selectedFile;
-    console.log("the file is", file)
+    console.log("the file is", file);
 
-    if (file) {
-      try {
-        const response = await fetch("http://localhost:5000/predict", {
-          // Your POST endpoint
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: file
-          // This is your file object
-        });
-        console.log("the response is", response);
-        const responseJson = response.json();
-        console.log("the response is", responseJson);
-      } catch (err) {
-        console.log(err)
-      }
-    }
+    // if (file) {
+    //   try {
+    //     const response = await fetch("http://localhost:5000/predict", {
+    //       // Your POST endpoint
+    //       method: "POST",
+    //       headers: { "Content-Type": "application/json" },
+    //       body: file
+    //       // This is your file object
+    //     });
+    //     console.log("the response is", response);
+    //     const responseJson = response.json();
+    //     console.log("the response is", responseJson);
+    //   } catch (err) {
+    //     console.log(err);
+    //   }
+    // }
   };
 
   handleselectedFile = event => {
@@ -79,26 +79,43 @@ class App extends Component {
 
           <p>Radiology prediction</p>
           <div>
+            <form
+              action="http://localhost:5000/uploader"
+              method="POST"
+              enctype="multipart/form-data"
+            >
 
-          <form action = "http://localhost:5000/uploader" method = "POST"
-         enctype = "multipart/form-data">
-         <input type = "file" name = "file" />
-         <input type = "submit"/>
-      </form>
+            <select>
+              <option value="Atelectasis">Atelectasis</option>
+              <option value="Cardiomegaly">Cardiomegaly</option>
+              <option value="Effusion">Effusion</option>
+              <option value="Infiltration">Infiltration</option>
+              <option value="Mass">Mass</option>
+              <option value="Nodule">Nodule</option>
+              <option selected value="Pneumonia">Pneumonia</option>
+              <option value="Pneumothorax">Pneumothorax</option>
+              <option value="Consolidation">Consolidation</option>
+              <option value="Edema">Edema</option>
+              <option value="Emphysema">Emphysema</option>
+              <option value="Fibrosis">Fibrosis</option>
+              <option value="Pleural_Thickening">Pleural_Thickening</option>
+              <option value="Hernia">Hernia</option>
+            </select>
+              <input type="file" name="file" onChange={this.handleselectedFile}/>
 
 
+              <input type="submit" onClick={this.handleUpload} />
+            </form>
 
-
-
-
-            <input
+            {/* <input
               type="file"
               name=""
               id=""
               onChange={this.handleselectedFile}
             />
 
-            <button onClick={this.handleUpload}>Make prediction</button>
+            <button onClick={this.handleUpload}>Make prediction</button> */}
+
           </div>
 
           <div className="Prediction-container">
@@ -120,21 +137,16 @@ class App extends Component {
             </div>
           )}
 
-
-
-      <div> <a target="_blank" href="https://arxiv.org/pdf/1711.05225.pdf"><p>Based on ChexNet</p></a>
-
-<div style={{overflow:'scroll',height:600}}>
-      <PDFReader url="https://arxiv.org/pdf/1711.05225.pdf"/>
-     </div>
-</div>
-
-
+          <div>
+            {" "}
+            <a target="_blank" href="https://arxiv.org/pdf/1711.05225.pdf">
+              <p>Based on ChexNet</p>
+            </a>
+            <div style={{ overflow: "scroll", height: 600 }}>
+              <PDFReader url="https://arxiv.org/pdf/1711.05225.pdf" />
+            </div>
+          </div>
         </div>
-
-        
-
-
       </div>
     );
   }
